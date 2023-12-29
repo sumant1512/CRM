47e3238e60a6fa585f1bbdcb0b20ec8193e62e4d
pankaj-patidar <41363752+pankaj-patidar@users.noreply.github.com>
2023-12-29 16:27:35 +0530

    Update wallet.controllers.js

--- server/backend/controller/wallet.controllers.js
+++ server/backend/controller/wallet.controllers.js
@@ -4,6 +4,8 @@ const { format } = require('date-fns');
 
 const { sendResponseError,incrementTransactionCount } = require("../middleware/middleware");
 
+
+
 const getwallet = async (req, res) => {
   const {user_id, admin_id} = req.query;
   const roleType = await incrementTransactionCount(user_id, admin_id,res)
@@ -114,15 +116,40 @@ const updatewallet = async (req, res) => {
 };
 
 const getwalletById = async (req, res) => {
-  console.log(req.params);
-  // try {
-  //   const product = await Product.findById(req.params.id);
+  const wallet_id = req.params.id
+  
+  const {user_id, admin_id} = req.query;
+  const roleType = await incrementTransactionCount(user_id, admin_id,res)
 
-  //   res.json(product);
-  // } catch (error) {
-  //   console.error(error);
-  //   res.status(500).json({ message: "Server Error" });
-  // }
+  if(roleType != "superadmin" && roleType != "employee"){
+    const getWalletsQuery =
+      "SELECT * FROM expenses_managment.wallet WHERE id=?";
+      try {
+          connectDB.query(getWalletsQuery,[wallet_id])
+          .then(([result]) =>{
+            if (result.length <=  0) {
+              
+              // return Promise.reject(new Error('Failed to delete expenses.'));
+              res.status(500).send({ status: false, message: "Failed to fetch wallet data."});
+              // Handle error or inform the user that the email already exists
+            } else {
+              res.status(200).send({ status: true, message: "Wallet data fetch succesfully.", data: result });
+            }
+          })
+          .catch(err => {
+              sendResponseError(500, "Error in fetching Wallet. Error- "+err.message,res);
+              
+            });
+      } catch (err) {
+        console.log(err)
+        sendResponseError(500, "Something went wrong. Please try again");
+      }
+  }
+  else{
+    res
+      .status(400)
+      .send({ status: false, message: "User is not admin. Feature is only for admin." });
+  }
 };
 
 module.exports = {
