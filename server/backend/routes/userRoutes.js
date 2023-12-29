92eb19d48e402fe0f8c49e147540aeb5a031b05f
pankaj-patidar <41363752+pankaj-patidar@users.noreply.github.com>
2023-12-29 18:32:57 +0530

    Update userRoutes.js

--- server/backend/routes/userRoutes.js
+++ server/backend/routes/userRoutes.js
@@ -1,14 +1,20 @@
 const express = require('express')
 const {
-  signUpUser,
-  signInUser,
+  registerUser,
+  loginUser,
   getUser,
+  activateUser,
+  resetPassword
 } = require('../controller/user.controller')
 const {verifyUser} = require('../middleware/middleware')
 const router = express.Router()
 
-router.post('/signup', signUpUser)
-router.post('/signin', signInUser)
+router.post('/register', registerUser)
+router.post('/login', loginUser)
+
+router.route('/activate/:id').get([verifyUser], activateUser )
+
+router.route('/reset-password').post([verifyUser], resetPassword )
 
 router.route('/me').get([verifyUser], getUser)
 
