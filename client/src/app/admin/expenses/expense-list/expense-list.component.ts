66db70d6545ff593109c04a3c50f216cca79f839
sumant1512 <sumantmishra511@gmail.com>
2024-01-01 00:22:53 +0530

    added more pages

--- client/src/app/admin/expenses/expense-list/expense-list.component.ts
+++ client/src/app/admin/expenses/expense-list/expense-list.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-expense-list',
   templateUrl: './expense-list.component.html',
-  styleUrls: ['./expense-list.component.scss']
+  styleUrls: ['./expense-list.component.scss'],
 })
 export class ExpenseListComponent {
-
+  expenseList: Array<any> = [];
 }
--- client/src/app/admin/expenses/expenses.module.ts
+++ client/src/app/admin/expenses/expenses.module.ts
@@ -5,9 +5,15 @@ import { ExpensesRoutingModule } from './expenses-routing.module';
 import { ExpensesComponent } from './expenses.component';
 import { ExpenseListComponent } from './expense-list/expense-list.component';
 import { AddExpenseComponent } from './add-expense/add-expense.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [ExpensesComponent, ExpenseListComponent, AddExpenseComponent],
-  imports: [CommonModule, ExpensesRoutingModule],
+  imports: [
+    CommonModule,
+    ExpensesRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
 })
 export class ExpensesModule {}
