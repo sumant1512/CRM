15bfc6466d6c2b5c0212c1b3d61bf9a8436ce579
sumant1512 <sumantmishra511@gmail.com>
2024-01-11 00:01:06 +0530

    add expenses for employee added

---
+++ client/src/app/employee/expenses/add-expense/add-expense.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { addExpenseForm } from './add-expense.form';
+import { TokenStorageService } from 'src/app/_services/token-storage.service';
+import { ExpensesService } from '../expenses.service';
+import { Subscription } from 'rxjs';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ICategory } from 'src/app/admin/category/category.interface';
+import { CategoryService } from 'src/app/admin/category/category.service';
+
+@Component({
+  selector: 'app-add-expense',
+  templateUrl: './add-expense.component.html',
+  styleUrls: ['./add-expense.component.scss'],
+})
+export class AddExpenseComponent implements OnInit, OnDestroy {
+  subscription = new Subscription();
+  addExpenseForm = addExpenseForm();
+  categoryList: Array<ICategory> = [];
+
+  constructor(
+    private expenseService: ExpensesService,
+    private categoryService: CategoryService,
+    private tokenService: TokenStorageService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.getCategoryList();
+  }
+
+  getCategoryList(): void {
+    const userInfo = this.tokenService.getUser();
+    const adminId = userInfo.roleId === 2 ? userInfo.id : userInfo.adminId;
+    this.subscription.add(
+      this.categoryService.fetchCategory(adminId).subscribe((response: any) => {
+        this.categoryList = response;
+      })
+    );
+  }
+
+  addExpense() {
+    const userInfo = this.tokenService.getUser();
+    const addExpenseBody = {
+      ...this.addExpenseForm.value,
+      adminId: userInfo.roleId === 2 ? userInfo.id : userInfo.adminId,
+      userId: userInfo.id,
+    };
+    this.subscription.add(
+      this.expenseService.addExpense(addExpenseBody).subscribe((response) => {
+        if (response.status) {
+          this.router.navigate(['../'], { relativeTo: this.activatedRoute });
+        }
+      })
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+}
