2c6a9e9a73aa9dbc4b5592ad78518e914b07e71c
sumant1512 <sumantmishra511@gmail.com>
2024-01-12 23:24:10 +0530

    added profile screen

--- client/src/app/employee/employee.module.ts
+++ client/src/app/employee/employee.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { EmployeeRoutingModule } from './employee-routing.module';
 import { EmployeeComponent } from './employee.component';
 import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
-  declarations: [EmployeeComponent, HomeComponent],
+  declarations: [EmployeeComponent, HomeComponent, ProfileComponent],
   imports: [CommonModule, EmployeeRoutingModule],
 })
 export class EmployeeModule {}
